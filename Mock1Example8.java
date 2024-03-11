91068a0e2a2907db7be2e6d521c56f41d74b5907
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-03-11 11:15:18 +0530

    use of switch

---
+++ Mock1Example8.java
@@ -0,0 +1,46 @@
+ import java.util.*;
+ 
+ 
+/* write a programm to  take user input number  and print the month name related to that of  user given number.*/
+import java.util.Scanner;
+class  Mock1Example8{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+System.out.println("enter number related to month");
+int a=sc.nextInt();
+switch(a){
+    case 1:System.out.println("Jan");
+    break;
+    case 2:System.out.println("Feb");
+    break;
+    case 3:System.out.println("March");
+    break;
+    case 4:System.out.println("April");
+    break;
+    case 5:System.out.println("May");
+    break;
+    case 6:System.out.println("June");
+    break;
+    case 7:System.out.println("July");
+    break;
+    case 8:System.out.println("August");
+    break;
+    case 9:System.out.println("Sep");
+    break;
+    case 10:System.out.println("Oct");
+    break;
+    case 11:System.out.println("Nov");
+    break;
+    case 12:System.out.println("Dec");
+    break;
+    default:System.out.println("entered number is invalid");
+}
+    }
+}
+
+/*output:
+enter number related to month
+4
+April
+
+*/
