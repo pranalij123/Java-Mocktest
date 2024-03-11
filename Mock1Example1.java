ac1a7b0461fd14577219b4def7974d58881e0b2a
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-03-11 10:49:33 +0530

    do while and use of Scanner class

---
+++ Mock1Example1.java
@@ -0,0 +1,27 @@
+ import java.util.*;
+ 
+ 
+ class Mock1Example1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+        int a=sc.nextInt();
+        int b=a;
+        int m=0;
+       do{
+       int  r=a%10;
+       m=m*10+r;
+       a=a/10;
+       }while(a>0);
+       if(b==m){
+        System.out.println("palindrom");
+       }
+        
+        
+    }
+}
+/*Output:
+Enter number
+22222
+palindrom
+*/
