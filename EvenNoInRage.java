2ad66dc4ef1152990d18c85e593dfd6ade33f95a
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-03-13 23:52:32 +0530

    Even No In Rage

---
+++ EvenNoInRage.java
@@ -0,0 +1,32 @@
+ 
+ //write programm to print first 10 even numbers
+ 
+ class EvenNoInRage {
+    public static void main(String[] args) {
+       int num=0;
+       int count=0;
+       while(true){
+        if(num%2==0){
+            System.out.println(num);
+            count++;
+        }
+        num++;
+        if(count==10){
+            break;
+        }
+       }
+    }
+}
+/*output:
+0
+2
+4
+6
+8
+10
+12
+14
+16
+18
+
+*/
