ec950be5c5c8dd8667cf78e2dc5b31a685876aa5
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-03-13 23:56:21 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+//write programm to print factorial of given number
+
+public class Rst {
+
+    public static void main(String[] args) {
+        
+int num=6;
+int fac=1;
+        while (num > 0) {
+             fac=num*fac;//6*5*4*3*2*1
+
+            num--;
+
+        }
+System.out.println(fac);
+
+    
+}
+}
+
+/*output:
+720
+*/
