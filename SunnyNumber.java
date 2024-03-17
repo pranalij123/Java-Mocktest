157a93eb6e4d8c0b0b9dca9b5b147f5fed8308f0
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-03-17 23:02:12 +0530

    Create  SunnyNumber.java

---
+++ SunnyNumber.java
@@ -0,0 +1,27 @@
+//A number is called a sunny number if the number next to the given number is a perfect square
+public class SunnyNumber {
+    public static void main(String[] args) {
+        
+        int n=80;
+        
+        int r=0;
+       for(int i=0;i<n;i++){
+        r=i*i;
+        int w=r-1;
+        if(w==n){
+            System.out.println("Its sunny number");
+            break;
+        }
+       }
+      if(i==n)
+      {
+System.out.println("It is not sunny number");
+      }
+
+        }
+}
+    
+
+/*output:
+Its sunny number
+*/
