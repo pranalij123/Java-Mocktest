4af249fb0e3601656d6f9d41b3e84acd4acb2aa2
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-04-17 11:30:45 +0530

    Create AutomorphicNo.java

---
+++ Methods/MethodsWithVoidReturn/AutomorphicNo.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+class AutomorphicNo{
+     public static void automorphic(int n){
+        int square=n*n;
+        int copy=n;
+        int no=0;
+        int count=0;
+        while (n>0) {
+            count++;
+            n/=10;
+        }
+        int c=count;
+        while (count>0) {
+            int rem=square%10;
+            no=no*10+rem;
+            count--;
+        }
+        int no1=0;
+        while(c>0){
+            int rem=no%10;
+            no1=no1*10+rem;
+            c--;
+        }
+        if(copy==no1){
+            System.out.println(copy+" is automorphic no");
+        }
+        else{
+            System.out.println(copy+" is not automorphic no");
+        }
+
+     }
+     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+         automorphic(n);
+    }
+}
+/*output:
+ Enter a number:
+5
+5 is automorphic no
+ */
