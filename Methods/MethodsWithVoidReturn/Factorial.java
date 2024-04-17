2d0032f9e9f0895cc757033f124cfa89a556b97e
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-04-17 10:27:27 +0530

    Update Factorial.java

--- Methods/MethodsWithVoidReturn/Factorial.java
+++ Methods/MethodsWithVoidReturn/Factorial.java
@@ -1,1 +1,22 @@
-
+//write programm to print factorial of given number
+import java.util.Scanner;
+class Factorial{
+    public static void fact(int n){
+        int factorial=1;
+        for(int i=n;i>=2;i--){
+            factorial=factorial*i;
+        }
+        System.out.println("Factorial of "+n+" is :"+factorial);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=sc.nextInt();
+        fact(n);
+    }
+}
+/*output:
+ Enter a number:
+3
+Factorial of 3 is :6
+ */
