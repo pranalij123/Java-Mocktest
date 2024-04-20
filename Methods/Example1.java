14cf5acfc15e116b31e937e4dcf0d139dab7b8fc
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-04-20 11:20:38 +0530

    Create Example1.java

---
+++ Methods/Example1.java
@@ -0,0 +1,15 @@
+
+
+class Example1{
+    public static void demo(){
+        System.out.println("hii from demo()");
+    }
+        public static void main(String[] args) {
+      demo();//directly called
+      Example.demo();//called with the help of class name as reference
+    }
+}
+/*output
+ hii from demo()
+hii from demo()
+ */
