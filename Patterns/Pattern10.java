23763c2444497aa7724c19fad4e3b22b43e72c33
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-04-20 11:28:26 +0530

    Update Pattern10.java

--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -1,1 +1,26 @@
 
+
+
+class Patten10{
+    
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<(5-i);j++){
+               System.out.print(" ");
+                
+            }
+            for(int j=0;j<i;j++){
+            System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*output
+    * 
+   * *
+  * * *
+ * * * *
+* * * * *
+
+ */
