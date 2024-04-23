f6f6a632fa402e2ee248dea8565b579eb740093c
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-04-23 15:41:15 +0530

    Create StaticVariable.java

---
+++ static/StaticVariable.java
@@ -0,0 +1,13 @@
+class StaticVariable{
+     static int a;
+    static {
+       a=10;
+    }
+    
+    public static void main(String args[]){
+    System.out.println(a);
+    }
+    }
+    /*output:
+     10
+     */
