fc60186a5b100c58986bba95f10931d5e63a4406
Pranali Jadhav <107744721+pranalij123@users.noreply.github.com>
2024-04-27 22:40:36 +0530

    Create Car.java

---
+++ nonstatic/Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    public static void main(String[] args) {
+        Car c=new Car();
+        System.out.println(c);
+        Car b= new Car();
+        System.out.println(b);
+    }
+}
+/*
+ Output:
+ Car@2f92e0f4
+Car@28a418fc
+ */
